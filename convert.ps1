5bc26d99ad83d93eb6ccf5716f194c5489650973
Tim <40317256+timlwsk@users.noreply.github.com>
2020-05-09 03:52:48 +0200

    Added varibles to ask for resolution and quality as well as check for folders

--- convert.ps1
+++ convert.ps1
@@ -1,3 +1,35 @@
-Get-ChildItem .\images_old\ | ForEach-Object {
-    .\cwebp.exe -q 100 -resize 1000 1000 $_.FullName -o .\images_new\$($_.BaseName).webp
+if ( -not (Test-Path .\imagesOld) ) {
+    Write-Host -ForegroundColor Yellow "folder imagesOld missing, creating"
+    New-Item -Name "imagesOld" -ItemType "directory" | Out-Null
+}
+
+if ( -not (Test-Path .\imagesNew) ) {
+    Write-Host -ForegroundColor Yellow "folder imagesNew missing, creating"
+    New-Item -Name "imagesNew" -ItemType "directory" | Out-Null
+}
+
+$xResDefault = 500
+$xResPromt = Read-Host "Provide output x-resolution (Default $xResDefault)"
+$xResPromt = ($xResDefault,$xResPromt)[[bool]$xResPromt]
+
+$yResDefault = 500
+$yResPromt = Read-Host "Provide output y-resolution (Default $yResDefault)"
+$yResPromt = ($yResDefault,$yResPromt)[[bool]$yResPromt]
+
+$qualityDefault = 100
+$qualityPromt = Read-Host "Desired quiality [0-100] (Default $qualityDefault)"
+$qualityPromt = ($qualityDefault,$qualityPromt)[[bool]$qualityPromt]
+
+$caption = "CONFIRMATION"
+$messagen = 'Are you sure you want to proceed using these settings?'
+$choices  = '&Yes', '&No'
+
+$confirmation = $Host.UI.PromptForChoice($caption, $message, $choices, 1)
+if ($confirmation -eq 0) {
+    Get-ChildItem .\imagesOld\ | ForEach-Object {
+        .\cwebp.exe -q $qualityPromt -resize $xResPromt $yResPromt $_.FullName -o .\imagesNew\$($_.BaseName).webp
+    }
+} else {
+    Write-Host -ForegroundColor DarkRed "please launch the script again to change your settings"
+    exit
 }
